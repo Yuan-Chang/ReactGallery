51b066352b2768d813074d926874a38f6c35788e
Yuan Chang <yuanchang_chang@intuit.com>
2018-09-21 12:42:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React-web-sample
+
+  * This is the react web sample to show how we put fond-end and back-end structures together.
+
+  * It integrated webpack, redux, redux-thunk and the npm run scripts to build production index.html.
---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Header from './Header';
+import Landing from './Landing';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect()(App);
